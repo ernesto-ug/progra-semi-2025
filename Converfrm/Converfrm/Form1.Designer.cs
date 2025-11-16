57720ed0a782a741632e7345b1d3c5f140c2b1e5
Ernesto <edgarernestoramirez82@gmail.com>
2025-11-16 17:50:47 -0600

    conversor

---
+++ Converfrm/Converfrm/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace Converfrm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTipo = new System.Windows.Forms.Label();
+            this.lblDe = new System.Windows.Forms.Label();
+            this.lblA = new System.Windows.Forms.Label();
+            this.lblCantidad = new System.Windows.Forms.Label();
+            this.lblRespuesta = new System.Windows.Forms.Label();
+            this.cboTipo = new System.Windows.Forms.ComboBox();
+            this.cboA = new System.Windows.Forms.ComboBox();
+            this.cboDe = new System.Windows.Forms.ComboBox();
+            this.txtCantidad = new System.Windows.Forms.TextBox();
+            this.btnConvertir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTipo
+            // 
+            this.lblTipo.AutoSize = true;
+            this.lblTipo.Font = new System.Drawing.Font("Microsoft YaHei", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTipo.Location = new System.Drawing.Point(52, 32);
+            this.lblTipo.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblTipo.Name = "lblTipo";
+            this.lblTipo.Size = new System.Drawing.Size(35, 16);
+            this.lblTipo.TabIndex = 0;
+            this.lblTipo.Text = "Tipo:";
+            // 
+            // lblDe
+            // 
+            this.lblDe.AutoSize = true;
+            this.lblDe.Font = new System.Drawing.Font("Microsoft YaHei", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDe.Location = new System.Drawing.Point(52, 72);
+            this.lblDe.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblDe.Name = "lblDe";
+            this.lblDe.Size = new System.Drawing.Size(24, 16);
+            this.lblDe.TabIndex = 1;
+            this.lblDe.Text = "de:";
+            // 
+            // lblA
+            // 
+            this.lblA.AutoSize = true;
+            this.lblA.Font = new System.Drawing.Font("Microsoft YaHei", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblA.Location = new System.Drawing.Point(52, 126);
+            this.lblA.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblA.Name = "lblA";
+            this.lblA.Size = new System.Drawing.Size(17, 16);
+            this.lblA.TabIndex = 2;
+            this.lblA.Text = "a:";
+            // 
+            // lblCantidad
+            // 
+            this.lblCantidad.AutoSize = true;
+            this.lblCantidad.Font = new System.Drawing.Font("Microsoft YaHei", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCantidad.Location = new System.Drawing.Point(52, 179);
+            this.lblCantidad.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblCantidad.Name = "lblCantidad";
+            this.lblCantidad.Size = new System.Drawing.Size(58, 16);
+            this.lblCantidad.TabIndex = 3;
+            this.lblCantidad.Text = "cantidad:";
+            // 
+            // lblRespuesta
+            // 
+            this.lblRespuesta.AutoSize = true;
+            this.lblRespuesta.Font = new System.Drawing.Font("Microsoft YaHei", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRespuesta.Location = new System.Drawing.Point(52, 227);
+            this.lblRespuesta.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblRespuesta.Name = "lblRespuesta";
+            this.lblRespuesta.Size = new System.Drawing.Size(60, 16);
+            this.lblRespuesta.TabIndex = 4;
+            this.lblRespuesta.Text = "respuesta";
+            // 
+            // cboTipo
+            // 
+            this.cboTipo.FormattingEnabled = true;
+            this.cboTipo.Items.AddRange(new object[] {
+            "Monedas",
+            "Longitud",
+            "Masa",
+            "Volumen",
+            "Almacenamiento",
+            "Tiempo",
+            "Area"});
+            this.cboTipo.Location = new System.Drawing.Point(103, 32);
+            this.cboTipo.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.cboTipo.Name = "cboTipo";
+            this.cboTipo.Size = new System.Drawing.Size(187, 21);
+            this.cboTipo.TabIndex = 5;
+            this.cboTipo.SelectedIndexChanged += new System.EventHandler(this.cboTipo_SelectedIndexChanged);
+            // 
+            // cboA
+            // 
+            this.cboA.FormattingEnabled = true;
+            this.cboA.Location = new System.Drawing.Point(103, 121);
+            this.cboA.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.cboA.Name = "cboA";
+            this.cboA.Size = new System.Drawing.Size(187, 21);
+            this.cboA.TabIndex = 6;
+            // 
+            // cboDe
+            // 
+            this.cboDe.FormattingEnabled = true;
+            this.cboDe.Location = new System.Drawing.Point(103, 72);
+            this.cboDe.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.cboDe.Name = "cboDe";
+            this.cboDe.Size = new System.Drawing.Size(187, 21);
+            this.cboDe.TabIndex = 7;
+            // 
+            // txtCantidad
+            // 
+            this.txtCantidad.Location = new System.Drawing.Point(103, 179);
+            this.txtCantidad.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtCantidad.Name = "txtCantidad";
+            this.txtCantidad.Size = new System.Drawing.Size(93, 20);
+            this.txtCantidad.TabIndex = 8;
+            // 
+            // btnConvertir
+            // 
+            this.btnConvertir.Font = new System.Drawing.Font("Microsoft YaHei", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConvertir.Location = new System.Drawing.Point(165, 227);
+            this.btnConvertir.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnConvertir.Name = "btnConvertir";
+            this.btnConvertir.Size = new System.Drawing.Size(103, 35);
+            this.btnConvertir.TabIndex = 9;
+            this.btnConvertir.Text = "Convertir";
+            this.btnConvertir.UseVisualStyleBackColor = true;
+            this.btnConvertir.Click += new System.EventHandler(this.btnConvertir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(454, 316);
+            this.Controls.Add(this.btnConvertir);
+            this.Controls.Add(this.txtCantidad);
+            this.Controls.Add(this.cboDe);
+            this.Controls.Add(this.cboA);
+            this.Controls.Add(this.cboTipo);
+            this.Controls.Add(this.lblRespuesta);
+            this.Controls.Add(this.lblCantidad);
+            this.Controls.Add(this.lblA);
+            this.Controls.Add(this.lblDe);
+            this.Controls.Add(this.lblTipo);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTipo;
+        private System.Windows.Forms.Label lblDe;
+        private System.Windows.Forms.Label lblA;
+        private System.Windows.Forms.Label lblCantidad;
+        private System.Windows.Forms.Label lblRespuesta;
+        private System.Windows.Forms.ComboBox cboTipo;
+        private System.Windows.Forms.ComboBox cboA;
+        private System.Windows.Forms.ComboBox cboDe;
+        private System.Windows.Forms.TextBox txtCantidad;
+        private System.Windows.Forms.Button btnConvertir;
+    }
+}
+
